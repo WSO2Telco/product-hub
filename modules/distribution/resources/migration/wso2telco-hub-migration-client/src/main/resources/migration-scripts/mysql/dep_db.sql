e3809c0deb53f04de6fe6a29790fd7f01c557f4c
nilan <nilan.rukshan@gmail.com>
2017-04-25 16:47:46 +0530

    INTGW-58 Migration script missing for newly added workflow_reference table in DEP DB

--- modules/distribution/resources/migration/wso2telco-hub-migration-client/src/main/resources/migration-scripts/mysql/dep_db.sql
+++ modules/distribution/resources/migration/wso2telco-hub-migration-client/src/main/resources/migration-scripts/mysql/dep_db.sql
@@ -1,7 +1,3 @@
-ALTER TABLE `<<STATS_DB>>`.`sub_approval_audit`  drop primary key;
-
-ALTER TABLE `<<STATS_DB>>`.`sub_approval_audit`  ADD SUB_APPROVAL_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
-
 CREATE TABLE IF NOT EXISTS `workflow_reference` (
   `workflow_ref_id` varchar(255) NOT NULL,
   `application_id` varchar(45) DEFAULT NULL,
---
+++ modules/distribution/resources/migration/wso2telco-hub-migration-client/src/main/resources/migration-scripts/mysql/stats_db.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `sub_approval_audit`  drop primary key;
+
+ALTER TABLE `sub_approval_audit`  ADD SUB_APPROVAL_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
